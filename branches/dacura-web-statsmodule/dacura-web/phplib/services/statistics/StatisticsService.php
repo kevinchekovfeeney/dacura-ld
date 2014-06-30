57ff2e7bfb19649449505f5a91aa1e3e4f4ab7d8
maxbrunner <maxbrunner@mycompany.com>
2014-06-30 13:17:08 +0000

    General code cleaning and comments for future documentation
    git-svn-id: https://phaedrus.cs.tcd.ie/svn/dacura@19 5c8ecfbf-1bd4-4697-a1fe-929c8cba0e57

--- branches/dacura-web-statsmodule/dacura-web/phplib/services/statistics/StatisticsService.php
+++ branches/dacura-web-statsmodule/dacura-web/phplib/services/statistics/StatisticsService.php
@@ -3,27 +3,25 @@ include_once("StatisticsDacuraServer.php");
 
 class StatisticsService extends DacuraService {
 	
-	//cid/did/users/userid
-	
 	function handlePageLoad(){
 		if(count($this->servicecall->args) > 0) {
 			$firstarg = array_shift($this->servicecall->args);
 			$secondarg = array_shift($this->servicecall->args);
 			$thirdarg = array_shift($this->servicecall->args);
-			if ($secondarg == null && $thirdarg == null) { // user call
+			if ($secondarg == null && $thirdarg == null) { // perform user call
 				$this->renderScreen("stats", array("userid" => $firstarg));
 			}
-			else if ($thirdarg == null) { // general dated
+			else if ($thirdarg == null) { // perform general dated
 				$this->renderScreen("stats", array("startdate" => gmdate("d.m.Y H:i", $firstarg), "enddate" => gmdate("d.m.Y H:i", $secondarg)));
 			}
-			else if ($secondarg == 'session')	{ // session log
+			else if ($secondarg == 'session')	{ // perform session log
 				$this->renderScreen("stats", array("userid" => $firstarg, "sessionid" => $thirdarg));
 			}
-			else { // user dated
+			else { // perform user dated
 				$this->renderScreen("stats", array("userid" => $firstarg, "startdate" => gmdate("d.m.Y H:i", $secondarg), "enddate" => gmdate("d.m.Y H:i", $thirdarg)));
 			}
 		}
-		else { // general undated
+		else { // perform general undated
 			$this->renderScreen("stats", array());
 		}
 	}
