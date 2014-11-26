f1b04ecfdb69e893505aa633c0069b592a4b4a8b
chekov <chekov@chekov.org>
2014-11-26 18:34:35 +0000

    Added in welcome screen with bug fixes

--- dacura-web/phplib/services/welcome/screens/view.php
+++ dacura-web/phplib/services/welcome/screens/view.php
@@ -1,10 +1,19 @@
-<div class="dacura-dashboard-panel-container">
-<div class="dacura-dashboard-panel" id="management-panel">
-	<a href='<?=$service->get_service_url("scraper")?>'>
-		<div class='dacura-dashboard-button' id='dacura-users-button' title="user management">
-			<img class='dacura-button-img' src="<?=$service->url("image", "buttons/seshat.png")?>">
-			<div class="dacura-button-title">Export Seshat Data</div>
+<div id='dashboard-container'>
+	<div id='dashboard-menu'>
+		<UL class='dashboard-menu'><li class='dashboard-menu-selected'>Welcome to Dacura</li></UL>
+	</div>
+<div id='dashboard-content'>
+		<div id='dashboard-tasks'>
+			<div class="dacura-dashboard-panel-container">
+			<div class="dacura-dashboard-panel" id="management-panel">
+				<a href='<?=$service->get_service_url("scraper")?>'>
+					<div class='dacura-dashboard-button' id='dacura-users-button' title="user management">
+						<img class='dacura-button-img' src="<?=$service->url("image", "buttons/seshat.png")?>">
+						<div class="dacura-button-title">Export Seshat Data</div>
+					</div>
+				</a>				
+			</div>
+			</div>
 		</div>
-	</a>				
-</div>
+	</div>
 </div>
