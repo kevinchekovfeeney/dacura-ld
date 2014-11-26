74a9ff02a606c4bfa603ecc31c93a208ed856c1b
chekov <chekov@chekov.org>
2014-11-26 18:44:59 +0000

    change seshat icon url

--- dacura-web/phplib/services/welcome/screens/view.php
+++ dacura-web/phplib/services/welcome/screens/view.php
@@ -8,7 +8,7 @@
 				<div class="dacura-dashboard-panel" id="management-panel">
 					<a href='<?=$service->get_service_url("scraper")?>'>
 						<div class='dacura-dashboard-button' id='dacura-users-button' title="user management">
-							<img class='dacura-button-img' src="<?=$service->url("image", "buttons/seshat-squatting.gif")?>">
+							<img class='dacura-button-img' src="<?=$service->url("image", "seshat-squatting.gif")?>">
 							<div class="dacura-button-title">Export Seshat Data</div>
 						</div>
 					</a>				
