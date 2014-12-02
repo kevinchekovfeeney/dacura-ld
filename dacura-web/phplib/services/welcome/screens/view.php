f23ce4bd7a9ed7154ec58ab887d46952dccff2ed
Kevin Chekov Feeney <chekov@chekov.org>
2014-12-02 13:49:51 +0000

    Added link to bookmaklet...

--- dacura-web/phplib/services/welcome/screens/view.php
+++ dacura-web/phplib/services/welcome/screens/view.php
@@ -10,6 +10,7 @@
                                         <p>This is the home page for the Seshat project on the Dacura Platform. As more tools are developed, they will appear on this screen.
                                         Please note that this page, and all of the seshat data, is private and access is limited to Seshat administrators.
                                         In order to use the tools above, you need to have a Seshat Administrator Role.</p>
+                                        <p>In order to use the Page Validator Tool, you need to drag this <a href="javascript:(function(){s=document.createElement('script');s.src='http://dacura.scss.tcd.ie/admin/rest/seshat/0/scraper/grabscript';document.body.appendChild(s);})();">Validator Bookmarklet Link</a>  to your browser's bookmark bar</p>
                                         </div>
                 <div id='dashboard-tasks'>
                         <h4>Available Dacura tools</h4>
