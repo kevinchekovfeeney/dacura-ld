142d5f839df656666080eae6acb898c86726e2c3
Gavin Mendel-Gleason <mendelgg@scss.tcd.ie>
2015-05-11 16:16:57 +0100

    Updates for testing

--- services/testInstanceUpdates.py
+++ services/testInstanceUpdates.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
 import requests
+import timeit
+import datetime
+import json
 
-SERVER = 'http://parvus:3020/'
-INSTANCE = SERVER + '/dacura/instance/'
-SCHEMA = SERVER + '/dacura/schema/'
+SERVER = 'http://parvus:3020'
+INSTANCE = SERVER + '/dacura/instance'
+SCHEMA = SERVER + '/dacura/schema'
 
 STARDOG = 'http://parvus:5822/sioc'
 
@@ -13,13 +16,6 @@ SIOC = 'http://rdfs.org/sioc/#'
 RDF = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
 NS = 'http://rdfs.org/sioc/ns#'
 PURL = 'http://purl.org/dc/terms/'
-    sioc:TalkAtT11-00-51     a st:InstantMessage;
-         <http://purl.org/dc/terms/issued> "2009-03-10T11:00:51Z";
-         :content "logger-sioc-rdfa has joined #swig";
-         :has_creator  [
-             a :UserAccount;
-             <http://xmlns.com/foaf/0.1/accountName> "logger-sioc-rdfa" ];
-         :link <http://www.swig.org/2009-03-10.html#T11-00-51> .
 FOAF = 'http://xmlns.com/foaf/0.1/accountName'
 
 counter = 0
@@ -32,45 +28,48 @@ def gensym(name):
 def new_instant_message():
     name = gensym('Message-')
     node = gensym('Node')
-    return
-    [[SIOC+name, RDF+'type', ST+'InstantMessage',instance],
-     [SIOC+name, PURL+'issued', datetime.datetime.now().isoformat()+"xsd:dateTime", instance],
-     [SIOC+name, NS+'content', "Some content here...", instance],
-     [SIOC+name, NS+'has_creator', node, instance],
-     [node, RDF+'type', NS+'UserAccount', instance],
-     [node,FOAF+'accountName', "An account name", instance],
-     [SIOC+name, NS+'link', 'http://www.swig.org/2009-03-10.html#' + name, instance]]
+    instance = 'instance'
+    return [
+        [SIOC+name, RDF+'type', ST+'InstantMessage',instance],
+        [SIOC+name, PURL+'issued', '"'+datetime.datetime.now().isoformat()+'"^xsd:dateTime',
+         instance],
+        [SIOC+name, NS+'content', '"Some content here..."', instance],
+        [SIOC+name, NS+'has_creator', node, instance],
+        [node, RDF+'type', NS+'UserAccount', instance],
+        [node,FOAF+'accountName', '"An account name"', instance],
+        [SIOC+name, NS+'link', 'http://www.swig.org/2009-03-10.html#' + name, instance]
+    ]
 
 def nMessages(n):
     triples = []
     for i in range(0, n):
-        triples.append(new_instant_message())
+        triples += new_instant_message()
     return triples
     
 def SPARQLise(triples):
     Query = """
     INSERT DATA
     { GRAPH sioc {"""
-    for triple in tripes:
+    for triple in triples:
         [x,y,z,g] = triple
         Query += " %s %s %s ." % (x,y,z)
     return Query+ "}}"
 
-
-def package(inserts, deletes):
-    return {"tests" : "all",
-            "inserts" : inserts,
-            "deletes" : deletes}
-
-#test_instance_json={"tests" : "all",
-#                    "inserts" : [[]]
-#                    "deletes" : [[]]
-#                    }
-
-
 def test_stardog_inserts():
-    r = requests.post(STARDOG)
-        
-def test_dacura_inserts():
-    r = requests.post(INSTANCE)
+    data = SPARQLise(nMessages(1000))
+    start = timeit.timeit()
+    r = requests.post(STARDOG, data=payload)
+    end = timeit.end()
+    print "Completed in time %s" % (start - end)
     
+def test_dacura_inserts():
+    data = nMessages(1000)
+    payload = {'pragma' : json.dumps({'tests' : 'all'}),
+               'update' : json.dumps({'inserts' : data,
+                                      'updates' : [],
+                                      'deletes' : []})}
+    start = timeit.timeit()
+    r = requests.post(INSTANCE, data=payload)
+    end = timeit.timeit()
+    print "Completed in time %s" % (start - end)
+    return r
