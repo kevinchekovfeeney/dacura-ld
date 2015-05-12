e161ec367a36e008bae4570a14d66ffb8c635748
Gavin Mendel-Gleason <mendelgg@scss.tcd.ie>
2015-05-12 17:44:23 +0100

    Dacura updates now functioning

--- services/testInstanceUpdates.py
+++ services/testInstanceUpdates.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import requests
-import timeit
+import time
 import datetime
 import json
 
@@ -16,7 +16,8 @@ SIOC = 'http://rdfs.org/sioc/#'
 RDF = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
 NS = 'http://rdfs.org/sioc/ns#'
 PURL = 'http://purl.org/dc/terms/'
-FOAF = 'http://xmlns.com/foaf/0.1/accountName'
+FOAF = 'http://xmlns.com/foaf/0.1/'
+XSD = 'http://www.w3.org/2001/XMLSchema#'
 
 counter = 0
 def gensym(name):
@@ -29,22 +30,30 @@ def new_instant_message():
     name = gensym('Message-')
     node = gensym('Node')
     instance = 'instance'
-    return [
-        [SIOC+name, RDF+'type', ST+'InstantMessage',instance],
-        [SIOC+name, PURL+'issued', '"'+datetime.datetime.now().isoformat()+'"^xsd:dateTime',
-         instance],
-        [SIOC+name, NS+'content', '"Some content here..."', instance],
-        [SIOC+name, NS+'has_creator', node, instance],
-        [node, RDF+'type', NS+'UserAccount', instance],
-        [node,FOAF+'accountName', '"An account name"', instance],
-        [SIOC+name, NS+'link', 'http://www.swig.org/2009-03-10.html#' + name, instance]
-    ]
+    return """
+        ['%(object)s', '%(type)s', '%(InstantMessage)s',instance],
+        ['%(object)s', '%(issued)s', literal(type('%(xsdDateTime)s','%(date)s')),instance],
+        ['%(object)s', '%(content)s', literal(lang('Some content here...', en)), instance],
+        ['%(object)s', '%(has_creator)s', '%(node)s', instance],
+        ['%(node)s', '%(type)s', '%(UserAccount)s', instance],
+        ['%(node)s','%(accountName)s', literal(lang('An account name',en)), instance],
+        ['%(object)s', '%(link)s', '%(href)s', instance]
+    """ % { "object" : SIOC+name, "node" : node, "type" : RDF+'type',
+             "content" : NS+'content',
+             "has_creator" : NS+'has_creator', 
+             "UserAccount" : NS+'UserAccount', "link" : NS+'link',
+             "href" : 'http://www.swig.org/2009-03-10.html#' + name,
+             "issued" : PURL+'issued',
+             "InstantMessage" : ST+'InstantMessage',
+             "date" : datetime.datetime.now().isoformat(),
+             "accountName" : FOAF+'accountName',
+             "xsdDateTime" : XSD + 'dateTime'}
 
 def nMessages(n):
-    triples = []
-    for i in range(0, n):
-        triples += new_instant_message()
-    return triples
+    triples = "[" + new_instant_message()
+    for i in range(1, n):
+        triples += ", " + new_instant_message()
+    return triples + "]"
     
 def SPARQLise(triples):
     Query = """
@@ -57,19 +66,20 @@ def SPARQLise(triples):
 
 def test_stardog_inserts():
     data = SPARQLise(nMessages(1000))
-    start = timeit.timeit()
+    start = time.time()
     r = requests.post(STARDOG, data=payload)
-    end = timeit.end()
+    end = time.time()
     print "Completed in time %s" % (start - end)
     
 def test_dacura_inserts():
-    data = nMessages(1000)
-    payload = {'pragma' : json.dumps({'tests' : 'all'}),
+    data = nMessages(10000)
+    payload = {'pragma' : json.dumps({'tests' : 'all',
+                                      'commit' : 'true'}),
                'update' : json.dumps({'inserts' : data,
                                       'updates' : [],
                                       'deletes' : []})}
-    start = timeit.timeit()
+    start = time.time()
     r = requests.post(INSTANCE, data=payload)
-    end = timeit.timeit()
-    print "Completed in time %s" % (start - end)
+    end = time.time()
+    print "Completed in time %s" % (end - start)
     return r
