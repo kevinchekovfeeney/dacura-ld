65a767432d6c4967019cf0ba0c641e0e735a3663
Gavin Mendel-Gleason <mendelgg@scss.tcd.ie>
2015-06-22 12:33:53 +0100

    Adding in finesse graphing code

--- services/finesse.py
+++ services/finesse.py
@@ -4,14 +4,21 @@ import glob
 import csv
 import re
 import datetime
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+import time
+import sys
 
+FIELDS = ['NGA', 'Polity', 'Section', 'Subsection', 'Variable', 'Value From', 'Value To', 'Date From', 'Date To', 'Fact Type', 'Value Note', 'Date Note', 'Comment', 'Area', 'Key', 'Value', 'Name','Error','Date', 'File']
 
-def run_aggregate(args): 
-    for f in glob.glob(args['glob']):
-        noHeader = True
-        with open('dated.tsv', 'ab') as tsvoutf:
+def run_aggregate(args):
+    with open('dated.tsv', 'wb') as tsvoutf:
+        
+        tsvout = csv.DictWriter(tsvoutf, delimiter='\t', fieldnames=FIELDS)
+        tsvout.writeheader()
+        
+        for f in glob.glob(args['glob']):
             m = re.match(".*([0-9]{2})([0-9]{2})([0-9]{4})T[0-9]{6}Z",f)
             if m:
                 year = m.group(3)
@@ -29,18 +36,13 @@ def run_aggregate(args):
         
             if m:
                 d = datetime.date(int(year),int(month),int(day))
+                ts = time.mktime(d.timetuple())
                 with open(f,'rb') as tsvinf:
                     tsvin = csv.DictReader(tsvinf, delimiter='\t')
                     
-                    if noHeader:
-                        header = tsvin.fieldnames + ["Date", "File"]
-                        tsvout = csv.DictWriter(tsvoutf, delimiter='\t', fieldnames=header)
-                        tsvout.writeheader()
-                        noHeader = False
-                    
                     for row in tsvin:
                         out = dict(row)
-                        out.update({'Date' : d.isoformat(), 'File' : f})
+                        out.update({'Date' : "%s" % ts, 'File' : f})
                         tsvout.writerow(out)
 
 def run_sort(args):
@@ -63,38 +65,192 @@ def run_sort(args):
 
 
 def run_plot(args):
-    with open('date-sorted.tsv', 'rb') as tsvinf:
+    with open('dated.tsv', 'rb') as tsvinf:
         tsvin = csv.DictReader(tsvinf, delimiter='\t')
-        
-        totals = []
-        last = None
-        count = 0
-        for row in tsvin:
-            current = row['File']
-            if current == last:
-                count += 1
-            else:
-                last = current
-                totals.append({'Current' : current, 'Count': count, 'Date' : row['Date']})
-                count = 1
-        # final element
-        totals.append({'Current' : current, 'Count': count, 'Date' : row['Date']})
 
-        counts = [row['Count'] for row in totals]
-        print counts
-        n_bins = len(totals)
+        counts = {}
+        for row in tsvin:
+            if row['File'] in counts:
+                (current_count,date,f) = counts[row['File']]
+                counts[row['File']] = (current_count + 1, date, f)
+            else:
+                counts[row['File']] = (1, float(row['Date']), row['File'])
+
+                
+        vals = counts.values()
+        vals.sort(key=lambda x: x[1])
         
-        colors = ['red', 'tan', 'lime']
-        n, bins, patches = plt.hist(counts, n_bins, normed=1, histtype='bar', color=colors, label=colors)
-        plt.plot(bins)
+        # Truncate small values
+        triples = [(x,y,f) for (x,y,f) in vals if x > 6200]
+        size = [x for (x,y,f) in triples]
+        dates = [ datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
+                  for (x,ts,f) in triples]
+        files = [ f for (x,ts,f) in triples]
+
+        N = len(size)
+        ind = np.arange(N)  # the x locations for the groups
+        width = 1       # the width of the bars
+
+        
+        fig, ax = plt.subplots()
+        rects1 = ax.bar(ind, size, width, color='r')
+        
+        # add some text for labels, title and axes ticks
+        ax.set_ylabel('Number of variables')
+        ax.set_title('Number of variables by date')
+        ax.set_xticks(ind+width)
+
+        #ax.set_xticklabels( files, rotation='vertical')
+        ax.set_xticklabels( dates, rotation='vertical')
+        plt.subplots_adjust(bottom=0.15)
+        #ax.legend((rects1[0],), ('Men',))
+
+        def autolabel(rects):
+             # attach some text labels
+             for rect in rects:
+                 height = rect.get_height()
+                 ax.text(rect.get_x()+.5, height - 1000, '%d'%int(height),
+                         #ha='center',
+                         va='top',
+                         rotation='vertical')
+
+        autolabel(rects1)
+
         plt.show()
+
+
+def run_polity_plot(args):
+    with open('dated.tsv', 'rb') as tsvinf:
+        tsvin = csv.DictReader(tsvinf, delimiter='\t')
+
+        # print tsvin.next()
+        # sys.exit(0)
+        
+        counts = {}
+        for row in tsvin:
+            if row['File']+row['NGA'] in counts:
+                (current_count,date,p,f) = counts[row['File']+row['NGA']]
+                counts[row['File']+row['NGA']] = (current_count + 1, date, p, f)
+            else:
+                counts[row['File']+row['NGA']] = (1, float(row['Date']), row['NGA'], row['File'])
+
+                
+        vals = counts.values()
+        vals.sort(key=lambda x: x[1]) # sort by date
+        vals.sort(key=lambda x: x[2]) # Sort by polity
+        
+        # Truncate small values
+        quads = [(x,y,p,f) for (x,y,p,f) in vals if x > 6200]
+        size = [x for (x,y,p,f) in quads]
+        labels = [ datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
+                  for (x,ts,polity,f) in quads]
+        files = [ f for (x,ts,polity,f) in quads]
+
+        N = len(size)
+        ind = np.arange(N)  # the x locations for the groups
+        width = 1       # the width of the bars
+
+        
+        fig, ax = plt.subplots()
+        rects1 = ax.bar(ind, size, width, color='r')
+        
+        # add some text for labels, title and axes ticks
+        ax.set_ylabel('Number of variables')
+        ax.set_title('Number of variables by date and polity')
+        ax.set_xticks(ind+width)
+
+        #ax.set_xticklabels( files, rotation='vertical')
+        ax.set_xticklabels( labels, rotation='vertical')
+        plt.subplots_adjust(bottom=0.15)
+        #ax.legend((rects1[0],), ('Men',))
+
+        def autolabel(rects):
+             # attach some text labels
+             count = 0
+             for rect in rects:
+                 height = rect.get_height()
+                 ax.text(rect.get_x()+.5, height - 1000, '%s' % quads[count][2],
+                         va='top',
+                         rotation='vertical')
+                 count += 1 
+
+
+        autolabel(rects1)
+
+        plt.show()
+
+
+def run_polity_plot(args):
+    with open('dated.tsv', 'rb') as tsvinf:
+        tsvin = csv.DictReader(tsvinf, delimiter='\t')
+
+        # print tsvin.next()
+        # sys.exit(0)
+        
+        counts = {}
+        for row in tsvin:
+            if row['File']+row['NGA'] in counts:
+                (current_count,date,p,f) = counts[row['File']+row['NGA']]
+                counts[row['File']+row['NGA']] = (current_count + 1, date, p, f)
+            else:
+                counts[row['File']+row['NGA']] = (1, float(row['Date']), row['NGA'], row['File'])
+
+                
+        vals = counts.values()
+        vals.sort(key=lambda x: x[1]) # sort by date
+        vals.sort(key=lambda x: x[2]) # Sort by polity
+        
+        # Truncate small values
+        quads = [(x,y,p,f) for (x,y,p,f) in vals if x > 6200]
+        size = [x for (x,y,p,f) in quads]
+        labels = [ datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
+                  for (x,ts,polity,f) in quads]
+        files = [ f for (x,ts,polity,f) in quads]
+
+        N = len(size)
+        ind = np.arange(N)  # the x locations for the groups
+        width = 1       # the width of the bars
+
+        
+        fig, ax = plt.subplots()
+        rects1 = ax.bar(ind, size, width, color='r')
+        
+        # add some text for labels, title and axes ticks
+        ax.set_ylabel('Number of variables')
+        ax.set_title('Number of variables by date and polity')
+        ax.set_xticks(ind+width)
+
+        #ax.set_xticklabels( files, rotation='vertical')
+        ax.set_xticklabels( labels, rotation='vertical')
+        plt.subplots_adjust(bottom=0.15)
+        #ax.legend((rects1[0],), ('Men',))
+
+        def autolabel(rects):
+             # attach some text labels
+             count = 0
+             for rect in rects:
+                 height = rect.get_height()
+                 ax.text(rect.get_x()+.5, height - 1000, '%s' % quads[count][2],
+                         va='top',
+                         rotation='vertical')
+                 count += 1 
+
+
+        autolabel(rects1)
+
+        plt.show()
+
+
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Generate dictionary and document set.')
-    parser.add_argument('--aggregate', '-a', help='Aggregate TSV files', default='./*.tsv')
+    parser.add_argument('--aggregate', '-a', help='Aggregate TSV files', action='store_true')
+    parser.add_argument('--glob', '-g', help='Files for aggregation', default='./*.tsv')
     parser.add_argument('--plot', '-p', help='Plot histograms', action='store_true')
+    parser.add_argument('--polity-plot', '-t', help='Plot polity histograms', action='store_true')
+    parser.add_argument('--polity-totals', '-o', help='Plot histograms of polity totals by date', action='store_true')
     parser.add_argument('--sort', '-s', help='Run sorting of files', action='store_true')
     args = vars(parser.parse_args())
 
@@ -110,3 +266,9 @@ if __name__ == '__main__':
         
     if 'plot' in args and args['plot']:
         run_plot(args)
+
+    if 'polity_plot' in args and args['polity_plot']:
+        run_polity_plot(args)
+
+    if 'polity_plot' in args and args['polity_totals']:
+        run_polity_totals(args)
