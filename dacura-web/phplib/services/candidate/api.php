9e93875c87ba7a68a302e339cbc20a7fd8325ca8
chekov <chekov@chekov.org>
2016-01-28 17:00:43 +0000

    stubs set up in candidate server

--- dacura-web/phplib/services/candidate/api.php
+++ dacura-web/phplib/services/candidate/api.php
@@ -1,5 +1,7 @@
 <?php
 getRoute()->get('/ngskeleton', 'get_ngskeleton');
+getRoute()->get('/entity_classes', 'get_entity_classes');
+getRoute()->get('/entity_frame/(\w+)', 'get_entity_frame');
 
 $entity_type = "candidate";
 
@@ -10,6 +12,26 @@ function get_ngskeleton(){
 	$skel = $dacura_server->getNGSkeleton();
 	if($skel){
 		return $dacura_server->write_json_result($skel, "Returned NG Skeleton");
+	}
+	$dacura_server->write_http_error();
+}
+
+function get_entity_classes(){
+	global $dacura_server;
+	$dacura_server->init("get.entity_classes");
+	$ents = $dacura_server->getCandidateEntityClasses();
+	if($ents){
+		return $dacura_server->write_json_result($ents, "Returned " . count($ents) . " " . $type. "s");
+	}
+	$dacura_server->write_http_error();
+}
+
+function get_entity_frame($entid){
+	global $dacura_server;
+	$dacura_server->init("get.entity_stub");
+	$frame = $dacura_server->getClassFrame($entid);
+	if($frame){
+		return $dacura_server->write_json_result($frame, "Returned $entid frame");
 	}
 	$dacura_server->write_http_error();
 }
