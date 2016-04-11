23fa5ecb0c5ed35aae40599bb0718592a24c3878
Gavin Mendel-Gleason <jacobian@gmail.com>
2016-04-11 14:16:19 +0100

    Adding some changes to javasript code

--- dacura-web/phplib/services/candidate/dacura.frame.js
+++ dacura-web/phplib/services/candidate/dacura.frame.js
@@ -23,21 +23,19 @@ dacura.frame.draw = function(resultobj, pconf){
 	
 	var elt = document.getElementById(pconf.busybox.substring(1));
 	elt.appendChild(res);
-	
+	return elt; 
 };
 
 dacura.frame.initInteractors = function (){
-	$('.queryInteractor').foreach(function(entry){
-		var id = $(entry).attr('id');
-		alert(id);
-
-
+	$('.queryInteractor').each(function(index){
+		var id = $(this).attr('id');
+		// put class search interactor here.
 	});
 		
 }
 
 dacura.frame.Gensym = function(pref){
-	var prefix = pref;
+	var prefix = pref + "-";
 	var count = 0;
 	return {
 		next: function(){
@@ -49,45 +47,68 @@ dacura.frame.Gensym = function(pref){
 };
 
 dacura.frame.frameGenerator = function(frame, obj, gensym){
-
+	//alert(JSON.stringify(frame,null,4));
 	/* list of frame options */ 
 	if(frame.constructor == Array){
-		// We are a property 
-		for (var i = 0 ; i < frame.length ; i++){
-			var property_elt = frame[i];
-			
-			var propdiv = document.createElement("div");
-			// propdiv.setAttribute('style', 'display: inline-block;');
-			var labelnode = document.createElement("label");
-			
-			if (property_elt.label){
-				var label = property_elt.label.data;
-				var textnode = document.createTextNode(label + ':');
-			}else{
-				var textnode = document.createTextNode(property_elt.property + ':');			
-			}
-			labelnode.appendChild(textnode);
-			propdiv.appendChild(labelnode);
-			
-			if( property_elt.type == 'objectProperty' ){
-				var subframe = property_elt.frame;
-				var framediv = document.createElement("div");
-				framediv.setAttribute('style', 'padding-left: 5px; display: inline-block;');
 
-				dacura.frame.frameGenerator(subframe, framediv, gensym);
-				labelnode.appendChild(framediv);
-			}else if(property_elt.type == 'datatypeProperty'){
-				var input = document.createElement("input");
-				var ty = dacura.frame.typeConvert(property_elt.range);
-				input.setAttribute('type', ty);
+		for (var i = 0 ; i < frame.length ; i++){
+			var elt = frame[i];
+
+			if( elt.type == "objectProperty" || elt.type == "datatypeProperty" ){
 				
-				labelnode.appendChild(input);
-			}
-			propdiv.appendChild(labelnode);
+				var propdiv = document.createElement("div");
+				if(elt.domain){
+					propdiv.setAttribute('data-class', elt.domain);
+				}else{
+					alert("No domain for: " + (elt.label
+											   || elt.property
+											   || JSON.stringify(elt, null, 4)));
+				}
 			
-			obj.appendChild(propdiv);
-		}
-	}else if(frame.constructor == Object && frame.type == 'entity'){
+				var labelnode = document.createElement("label");
+			
+				if (elt.label){
+					var label = elt.label.data;
+					var textnode = document.createTextNode(label + ':');				
+				}else{
+					var textnode = document.createTextNode(elt.property + ':');			
+				}
+				var divlabel = document.createElement("div");
+				divlabel.setAttribute('data-property', elt.property);
+				divlabel.setAttribute('style', 'float: left');
+				divlabel.appendChild(textnode); 
+				labelnode.appendChild(divlabel);
+				propdiv.appendChild(labelnode);
+				
+				if( elt.type == 'objectProperty' ){
+					var subframe = elt.frame;
+					var framediv = document.createElement("div");
+					framediv.setAttribute('style', 'padding-left: 5px; display: inline-block;');
+					
+					dacura.frame.frameGenerator(subframe, framediv, gensym);
+					labelnode.appendChild(framediv);
+				}else if(elt.type == 'datatypeProperty'){
+					var input = document.createElement("input");
+					var ty = dacura.frame.typeConvert(elt.range);
+					input.setAttribute('type', ty);
+					
+					labelnode.appendChild(input);
+				}else{
+					alert("Impossible: must be either an object or datatype property.");
+				}
+				
+				propdiv.appendChild(labelnode);				
+				obj.appendChild(propdiv);
+
+			}else if(elt.type == 'failure'){
+				//alert(elt.message + ' class :' + elt.domain);
+				alert(JSON.stringify(elt,null,4));
+			}else{
+				alert(JSON.stringify(elt,null,4)); 
+				//alert("What in gods name are we?");
+			}
+		} // for loop
+	}else if(frame.constructor == Object && frame.type == 'entity' || frame.type == 'thing' ){
 		// we are an entity
 		
 		var input = document.createElement("input");
@@ -96,10 +117,17 @@ dacura.frame.frameGenerator = function(frame, obj, gensym){
 		// This should really be a specialised search box.
 		input.setAttribute('type', 'text');
 		obj.appendChild(input);
+	}else{
+		alert("We are neither a property nor an entity:" + JSON.stringify(frame, null, 4));
 	}
 	
-	return obj; 
+	return obj;
+	
 };
+
+dacura.frame.entityExtractor = function(div){
+	// Extracts the entity graph as JSON from a div. 
+}
 
 dacura.frame.typeConvert = function(ty){
 	// This needs to be extended at each XSD type. 
@@ -111,8 +139,8 @@ dacura.frame.typeConvert = function(ty){
 	}
 };
 
-dacura.frame.fill = function(frameid, candidate){
-	//
+dacura.frame.fill = function(entity, target){
+	
 };
 
 dacura.frame.init = function(entity, pconf){
@@ -142,7 +170,8 @@ dacura.frame.init = function(entity, pconf){
 	var ajs = dacura.frame.api.getFrame(cls);
 	msgs = { "success": "Retrieved frame for "+cls + " class from server", "busy": "retrieving frame for "+cls + " class from server", "fail": "Failed to retrieve frame for class " + cls + " from server"};
 	ajs.handleResult = function(resultobj, pconf){
-		dacura.frame.draw(resultobj, pconf);
+		var frameid = dacura.frame.draw(resultobj, pconf);
+		dacura.frame.fill(resultobj, pconf); 
 		dacura.frame.initInteractors();
 	}
 	
